56b0d124d47495367d19a9c740996373e9493ab6
Michael Snowdon <151382049+michaelesnowdon@users.noreply.github.com>
2024-01-28 13:15:45 +0000

    add get a book function to book routes

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -193,10 +193,33 @@ const deleteBookForUser = async (req, res) => {
   }
 };
 
+const getBookById = async (req, res) => {
+  try {
+    const bookId = req.params.bookId; // ISBN of the book
+
+    // Use Prisma to find a book by its ISBN
+    const book = await prisma.book.findUnique({
+      where: {
+        isbn: bookId,
+      },
+    });
+
+    if (!book) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+
+    res.status(200).json(book);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while fetching the book");
+  }
+};
+
 module.exports = {
   getAllUnreadBooksForUser,
   getAllReadBooksForUser,
   postBookForUser,
   updateIsReadForUser,
   deleteBookForUser,
+  getBookById,
 };
--- routes/book-routes.js
+++ routes/book-routes.js
@@ -4,6 +4,7 @@ const bookController = require("../controllers/book-controller");
 
 router.get("/:userId/unread", bookController.getAllUnreadBooksForUser);
 router.get("/:userId/read", bookController.getAllReadBooksForUser);
+router.get("/:bookId", bookController.getBookById);
 router.post("/", bookController.postBookForUser);
 router.put("/:userId/update/:bookId", bookController.updateIsReadForUser);
 router.delete("/:userId/delete/:bookId", bookController.deleteBookForUser);
